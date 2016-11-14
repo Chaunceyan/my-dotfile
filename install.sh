fb96e09473e4ba39f7f97147aa1609968e82bfae
Yan Chunxiang <yanchunxiang@Yans-MacBook-Air.local>
2016-11-14 14:27:18 +0800

    Mac Configuration

--- install.sh
+++ install.sh
@@ -1,2 +1,27 @@
-ln -sv ~/Documents/dotfiles/.vimrc ~
-ln -sv ~/Documents/dotfiles/.zshrc ~
+#!/bin/bash
+
+if [[ `uname` == 'Darwin' ]]; then
+  # MacOS
+  # Install homebrew
+  /usr/bin/ruby -e \
+  "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+  # Install vim
+  brew install macvim --override-system-vim
+
+  # Install oh my zsh
+  sh -c \
+  "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+  # Install vundle
+  brew install git
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+  # Get vimrc
+  ln -svf ./.vimrc ~
+  ln -svf ./.zshrc ~
+
+else
+  # Linux
+  echo "Linux"
+fi
